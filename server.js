54736b3960cc504baadaaa4ddb8ef78237a00cf0
Kyle Weisse <kyle.weisse@gmail.com>
2021-07-23 11:29:49 -0400

    access filePath and pipe

--- server.js
+++ server.js
@@ -14,6 +14,29 @@ const server = http.createServer((req, res) => {
         if (fileUrl === '/') {
             fileUrl = './index.html';
         }
+
+        const filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt === '.html') {
+            fs.access(filePath, err => {
+                if (err) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-type', 'text/html');
+
+                fs.createReadStream(filePath).pipe(res); //createReadStream is lazy loading, so we load one thing at a time instead of all at once.
+                //pipe "pipes" data from one file to the other, like an actual pipe.
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: ${fileUrl} is not an html file</h1></body></html>');
+        }
+
     } else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/html');
